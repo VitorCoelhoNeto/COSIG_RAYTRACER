a625eca9613708938e698c3f3a3e977354405f62
Vítor Neto <vitordiogo2000@sapo.pt>
2022-05-11 21:54:38 +0100

    Added class example

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+class Color3:
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+    
+    def print_colors(self):
+        print(self.red, self.green, self.blue)
+
 def main():
-    print("A")
+    test = Color3(250, 230, 100)
+
+    test.print_colors()
 
 if __name__ == "__main__":
     main()
