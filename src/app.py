9fd64054b605ac4fab6c8090c00c42bb6c8c5dcd
Vítor Neto <vitordiogo2000@sapo.pt>
2022-05-15 22:07:12 +0100

    Added parser for the images segment

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
+import sys
+import json
 from raytracerGUI  import *
 from classes import *
-import sys
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
@@ -13,6 +14,30 @@ def window():
 
 
 def parser(path):
+
+    imageContents = {"Images":{}}
+
     with open(path) as file:
-        for line in file.readlines():
-            pass
+
+        imgNumber = 0
+
+        IMG_X     = 0
+        IMG_Y     = 1
+        IMG_RED   = 0
+        IMG_GREEN = 1
+        IMG_BLUE  = 2
+        
+        lines = file.readlines()
+
+        for iteration, line in enumerate(lines):
+
+            if "Image" in line:
+                imgNumber += 1
+                if imgNumber > 1:
+                    raise Exception("There may be only one segment of type 'Images'")
+                res = lines[iteration + 2].strip().split()
+                colors = lines[iteration + 3].strip().split()
+                imageContents["Images"].update({imgNumber:{"Resolution":{"X":res[IMG_X], "Y":res[IMG_Y]}, "BG_Color":{"Red":colors[IMG_RED], "Green":colors[IMG_GREEN], "Blue":colors[IMG_BLUE]}}})
+    
+    with open("temp.txt", "w", encoding="utf-8") as file2:
+        file2.write(json.dumps(imageContents, indent=5, separators=(',', ':')))
